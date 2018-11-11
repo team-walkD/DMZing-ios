c09de5a8a64c9f41696485c1f7e173de7fc9b94d
강수진 <rkdthd1234@naver.com>
2018-11-11 17:21:51 +0900

    마이페이지 리뷰통신

--- DMZing/DMZing/Mypage/Review/VC/MyReviewVC.swift
+++ DMZing/DMZing/Mypage/Review/VC/MyReviewVC.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
-class MyReviewVC: UIViewController {
+class MyReviewVC: UIViewController, APIService {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var mainData : [MypageReviewVOData] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
         setupNavBar()
+        getMainData(url: url("users/reviews"))
     }
     
     func setupTableView(){
@@ -32,14 +38,36 @@ class MyReviewVC: UIViewController {
 
 extension MyReviewVC : UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return mainData.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: MyReviewTVCell.reuseIdentifier) as! MyReviewTVCell
+        guard mainData.count > 0 else {return cell}
+        cell.configure(data: mainData[indexPath.row])
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension MyReviewVC {
+    func getMainData(url : String){
+        MypageReviewService.shareInstance.getMainData(url: url,completion: { [weak self] (result) in
+            guard let `self` = self else { return }
+            switch result {
+            case .networkSuccess(let data):
+                let mainData = data as? MypageReviewVO
+                if let mainData_ = mainData {
+                    self.mainData = mainData_
+                }
+            case .networkFail :
+                self.networkSimpleAlert()
+            default :
+                self.simpleAlert(title: "오류", message: "다시 시도해주세요")
+                break
+            }
+        })
     }
 }
