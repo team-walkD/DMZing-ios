4a07d2f93c2fa5956d351611b28985736f915142
장용범 <dnwkqk@gmail.com>
2018-11-13 22:22:10 +0900

    update main view

---
+++ DMZing/DMZing/Main/Model/FirstModel.swift
@@ -0,0 +1,19 @@
+//
+//  FirstModel.swift
+//  DMZing
+//
+//  Created by 장한솔 on 2018. 11. 13..
+//  Copyright © 2018년 장용범. All rights reserved.
+//
+
+import Foundation
+
+struct FirstModel{
+    var theme: String
+    var subtitle: String
+    var title: String
+    var titleImg: String
+    var difficulty: String
+    var time: String
+    var people: String
+}
